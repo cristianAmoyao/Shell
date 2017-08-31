cd314f6d4f5ebb4948aedca89a5759833a8da1e1
Pajaro_xdd <pabloalvarezcordoba@protonmail.ch>
2017-08-31 17:15:09 +0200

    reparar.sh 	Ya no hace falta la intervención del usuario, se obtiene el nombre del usuario y su home automáticamente. Antes había que ejecutar el script desde el mismo directorio que .ICEauthority, ya no. Antes se intentaba algo tipo: "chown _usuario_ foo" ahora "chown usuario foo".

--- Error_de_Su sesión_ha_durado_menos_de_10_segundos.sh
+++ Error_de_Su sesión_ha_durado_menos_de_10_segundos.sh
@@ -1,48 +1,10 @@
 #!/bin/bash
+
 clear
-echo "_________licencia GPL 3.0_________"
-echo "=================================="
-echo "==========Ayuda==================="
-echo "r = read (lectura) "
-echo "w = write(escritura)"
-echo "x = execution (ejecucion)"
-echo "=================================="
-echo "=================================="
-echo "   "
-ls -l .ICE*
-echo "   "
-echo "   "
-echo "Pregunta:"
-echo "   "
-echo "¿aparece esto?"
-echo "-rw------- "
-echo "¿o similar?"
-echo "===Opciones===="
-echo "1) si"
-echo "2) no"
-echo -n "Respuesta:  "
-
-read opcion 
-case $opcion in
-1)echo "su usuario es : $USER";
-read -p "¿Cual es su usuario?:  " user
-echo "sudo chown _"$user"_ .ICEauthority" >>reparar.sh;
-echo "   ";
-clear
-echo "se ejecutara el siguiente comando";cat reparar.sh;
-echo "iniciando script..."
-echo "para cancelar presione Ctrl + c"
-sleep 1
-echo "3"
-sleep 1
-echo "2"
-sleep 1
-echo "1"
-sleep 1
-sh reparar.sh ;;
-
-
-2)exit ;;
-esac
-
-
+if [ $(id -u) != "0" ] || [ ! $SUDO_USER ]; then
+	echo "Por favor, ejecuta éste script con sudo. :)"
+	exit 1
+fi
+echo -n "\n_________licencia GPL 3.0_________"
+HOME=$(cat /etc/passwd | grep $SUDO_USER | awk -F: '{print $6}')
+chown $SUDO_USER $HOME/.ICEauthority
